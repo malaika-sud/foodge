bc370a93cb089edcbcd363b2b983c44d100ec5f7
Malaika Sud <malaikasud@Malaikas-MacBook-Pro.local>
2023-07-13 14:36:20 -0700

    basic display to work off of, showcases mealdb recipes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { NavBar } from "./components/NavBar";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=c')
+      .then(response => response.json())
+      .then(data => {
+        setRecipes(data.meals);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App app-color">
+      <NavBar/>
+      <div className="title">
+        <h1>Foodge</h1>
+        <h2>your online cookbook.</h2>
+      </div>
+      <div id="recipeContainer">
+        {recipes.map(recipe => (
+          <div className="recipe" key={recipe.idMeal}>
+            <h3>{recipe.strMeal}</h3>
+            <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+            <div>
+              {recipe.strInstructions.split('. ').map((sentence, index) => (
+                <p key={index}>{sentence}</p>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
